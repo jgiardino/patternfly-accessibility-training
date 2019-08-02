bd448edf63e7680f6f6ecb0730690cdca0c6b7ac
Michael Spaxman <seanforyou23@gmail.com>
2019-08-02 11:04:36 -0400

    [cleanup] add missing stylesheets, tweak webpack config

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { AppLayout } from '@app/AppLayout/AppLayout';
 import { AppRoutes } from '@app/routes';
 import DocumentTitle from 'react-document-title';
+import '@patternfly/patternfly/patternfly.css';
+import '@patternfly/react-styles/css/utilities/Spacing/spacing.css';
+import '@patternfly/react-styles/css/utilities/Sizing/sizing.css';
+import '@patternfly/react-styles/css/utilities/Alignment/alignment.css';
+import '@patternfly/react-styles/css/utilities/Flex/flex.css';
+import '@patternfly/react-styles/css/layouts/Flex/flex.css';
 import '@app/app.css';
 
 const App: React.FunctionComponent = () => {
